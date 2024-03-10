1bc38b1ae58a0da21809868280edd5d199ae2b45
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2024-03-10 14:02:20 +0000

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateForm(event) {
+  event.preventDefault();
 
+  new Typewriter("#generated-response", {
+    strings: "Placeholder",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#generate-form");
+formElement.addEventListener("submit", generateForm);
